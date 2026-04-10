5068311ac30140df4a8d5f3374a81ac3a477342e
Ishini <ishinikavishka422@gmail.com>
2026-04-10 14:49:19 +0530

    Initial Commit

---
+++ backend/src/main/java/backend/BackendApplication.java
@@ -0,0 +1,23 @@
+package backend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(MongoTemplate mongoTemplate) {
+		return args -> {
+			System.out.println("✅ Connected to MongoDB: " + mongoTemplate.getDb().getName());
+			System.out.println("✅ Smart Campus Backend Started Successfully!");
+		};
+	}
+}
